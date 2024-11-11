ad6bfde169d688c616b1472211c19cce84ef36c1
Mubeshira <mubeshira.saad2016@hotmail.com>
2024-11-11 22:29:23 +0500

    updated commit

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -98,6 +98,24 @@ const projects = [
     imageUrl: "/adventure.jpg",
     link: "https://github.com/Mubeshira/Adventure-game-1.git",
   },
+  {
+    title: "Project 4",
+    description: "This is a pixel perfect panacloud website.",
+    imageUrl: "/pana.png",
+    link: "https://assignment12-panacloud.vercel.app/",
+  },
+  {
+    title: "Project 5",
+    description: "This is a terminal based currency converter app.",
+    imageUrl: "/exchange.jpg",
+    link: "https://github.com/Mubeshira/Currency-Converter-Application.git",
+  },
+  {
+    title: "Project 6",
+    description: "This is a terminal based currency converter app.",
+    imageUrl: "/qalculate2.webp",
+    link: "https://github.com/Mubeshira/simple_calculator.git",
+  },
   
 
 ];
@@ -112,20 +130,20 @@ export default function Projects() {
 
   return (
     <section className="py-16 bg-black min-h-screen">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8  ">
         <h2 className="text-4xl sm:text-5xl text-blue-700 font-bold text-center mb-8">Projects</h2>
-        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ">
           {projects.map((project, index) => (
             <a
               key={index}
               href={project.link}
               target="_blank"
               rel="noopener noreferrer"
-              className="bg-gray-300 shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-105"
+              className="bg-gray-300 shadow-lg rounded-lg overflow-hidden transition-transform transform hover:scale-120 "
               data-aos="fade-up"
               data-aos-delay={index * 100}
             >
-              <div className="relative h-48 sm:h-64">
+              <div className="relative h-48 sm:h-64 transition ease-in-out delay-150 bg-slate-500 hover:scale-105 duration-300">
                 <Image
                   src={project.imageUrl}
                   alt={project.title}
@@ -136,7 +154,7 @@ export default function Projects() {
               </div>
               <div className="p-4">
                 <h3 className="text-lg sm:text-xl font-semibold">{project.title}</h3>
-                <p className="text-gray-600 mt-2">{project.description}</p>
+                <p className="text-gray-600 mt-2 transition ease-in-out delay-150">{project.description}</p>
               </div>
             </a>
           ))}
